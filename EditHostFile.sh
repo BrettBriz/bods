41521a8928e780b2543f8f4761e9db2c05d5af57
Brett <BrettBriz@users.noreply.github.com>
2019-03-27 16:14:18 -0700

    Update EditHostFile.sh

--- EditHostFile.sh
+++ EditHostFile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "23.200.242.123 http://www-tbbuat.beachbody.com" >> /etc/hosts
+echo "23.200.242.123 http://www-tbbuat.beachbody.com/shop/" >> /etc/hosts
+echo "23.200.242.123 http://www-shareacartuat.beachbody.com" >> /etc/hosts
