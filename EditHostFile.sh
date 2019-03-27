6a33971d5cbd3724da4b29ef1ee95e023bc75cf6
Brett <BrettBriz@users.noreply.github.com>
2019-03-27 13:28:05 -0700

    Update EditHostFile.sh

--- EditHostFile.sh
+++ EditHostFile.sh
@@ -1,4 +1,2 @@
-SET NEWLINE=^& echo.
-
-FIND /C /I "www-tbbuat.beachbody.com" %WINDIR%\system32\drivers\etc\hosts
-IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%^23.200.242.123 www-tbbuat.beachbody.com>>%WINDIR%\System32\drivers\etc\hosts
+#!/bin/bash
+echo "23.200.242.123 http://www-tbbuat.beachbody.com" >> /etc/hosts
